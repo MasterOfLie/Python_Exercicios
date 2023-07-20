d44ccaaa5ef06ea265f5dd99f5e766593420b3c0
Matheus <mhapessoal@gmail.com>
2023-07-20 16:22:22 -0300

    Exercicios Lista

--- Estrutura De Decisao/19.py
+++ Estrutura De Decisao/19.py
@@ -1,1 +1,5 @@
 # 19 - Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e unidades do mesmo. 
+n = int(input('Digite um número inteiro menor que 1000: '))
+print('Quantidade de centenas: ', n // 100)
+print('Quantidade de dezenas: ', (n % 100) // 10)
+print('Quantidade de unidades: ', n % 10)
